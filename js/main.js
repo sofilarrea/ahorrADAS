12c3c017685a020deec4cd20e2c231bfe0cf3efc
sofilarrea <sofialejandra.larrea@gmail.com>
2024-04-15 16:10:23 -0300

    navbar style

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* Abrir y cerrar navbar */
+document.addEventListener('DOMContentLoaded', () => {
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const navbarLinks = document.getElementById('navbar-links');
+
+  open.addEventListener('click', () => {
+      open.style.display = 'none';
+      close.style.display = 'block';
+      navbarLinks.classList.add('show-nav');
+  });
+
+  close.addEventListener('click', () => {
+      open.style.display = 'block';
+      close.style.display = 'none';
+      navbarLinks.classList.remove('show-nav');
+  });
+});
