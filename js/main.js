30bf313e678af5009d206ae6d409bf84eb5fa1c8
sofilarrea <sofialejandra.larrea@gmail.com>
2024-04-25 23:11:47 -0300

    agregar gasto, falta editar y eliminar

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
       navbarLinks.classList.remove('show-nav');
   });
 });
+
+
+/* Categorías */
+const textInput = document.getElementById('text-input');
+const agregar = document.getElementById('agregar');
+const listaGastos = document.getElementById('lista-gastos');
+
+agregar.addEventListener('click', agregarGasto);
+
+function agregarGasto() {
+  const texto = textInput.value.trim();
+
+  if (texto === '') {
+    alert('Por favor, ingresa un nombre válido.');
+    return;
+  }
+
+  const nuevoGasto = document.createElement('div');
+  nuevoGasto.classList.add('nuevo-gasto');
+
+  const nuevoTexto = document.createElement('h2');
+  nuevoTexto.textContent = texto;
+
+  nuevoGasto.appendChild(nuevoTexto);
+
+  listaGastos.appendChild(nuevoGasto);
+
+  textInput.value = '';
+}
---
+++ js/tailwind.config.js
@@ -0,0 +1,21 @@
+/* tailwind.config ={
+  theme: {
+    extend:{
+      colors: {
+        greenBtn: '#48C774',
+      }
+    }
+  }
+}
+ */
+
+module.exports = {
+  theme: {
+    extend: {
+      colors:{
+        greenBtn: '#48C774',
+      }
+    },
+  },
+  plugins: [],
+}
